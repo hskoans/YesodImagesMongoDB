8174ef0689cfb5d2bee993919b6189c0c76a0e50
Calvin Cheng <linchuan.cheng@gmail.com>
2015-07-06 01:57:53 +0000

    Images route

---
+++ Handler/Images.hs
@@ -0,0 +1,9 @@
+module Handler.Images where
+
+import Import
+
+getImagesR :: Handler ()
+getImagesR = putStrLn "getting images"
+
+postImagesR :: Handler ()
+postImagesR = putStrLn "posting images"
